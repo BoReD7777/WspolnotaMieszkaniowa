fc8d16e119a16760839307ed195a26dbbc7398a9
BoReD7777 <bozekjakub8@gmail.com>
2023-11-23 18:03:34 +0100

    cost manager

--- src/package1/ActivityAndExpenses/CommunityServices.java
+++ src/package1/ActivityAndExpenses/CommunityServices.java
@@ -1,4 +1,4 @@
-package package1.Model;
+package package1.House;
 
 public class CommunityServices {
 }
--- src/package1/ActivityAndExpenses/CostManager.java
+++ src/package1/ActivityAndExpenses/CostManager.java
@@ -1,4 +1,4 @@
-package package1.Model;
+package package1.House;
 
 import java.util.Scanner;
 
